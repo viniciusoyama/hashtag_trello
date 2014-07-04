e2dfbc3b995cc6690181062b39add15a425e62e6
Vinícius Oyama <vinicius.oyama@codus.com.br>
2014-07-04 16:06:28 -0300

    changed logic to only update the tag list

--- hashtagrtello.js
+++ hashtagrtello.js
@@ -1,7 +1,8 @@
 var HASHTAG_REGEX = /\B#(\w*[a-zA-Z]+-*\w*)/;
 var MUSTACHE_REGEX = /{{(\s*[\-\w\.]+\s*)}}/ig;
+
 function replaceTextMustachesWithHtmlElements() {
-  var tagListHtml = "<div class='cds-hashtag-list'>";
+  var tagsHtml = "";
   // we must be carefull because trello has a hidden children element with #idcard
   this.contents().filter(function() {
     return this.nodeType === 3;
@@ -10,20 +11,29 @@ function replaceTextMustachesWithHtmlElements() {
     var cardTitle = $(this).text();
     var cardTitleHtml = cardTitle.replace(MUSTACHE_REGEX, function(mustacheElem) {
       var tag = mustacheElem.replace('{{', '').replace('}}', '');
-      tagListHtml += "<span class='cds-hashtag green'>" + tag + "</span>";
+      tagsHtml += "<span class='cds-tag green'>" + tag + "</span>";
       return "";
     });
     return cardTitleHtml;
   });
-  tagListHtml += "</div>";
-  $(tagListHtml).insertAfter(this);
+
+  $(this).parent().find(".js-cds-tag-list").html(tagsHtml);
 }
-function convertHashtagsToBadge() {
+
+function convertTagsToBadge() {
   $(".list-card-title").each(function() {
     replaceTextMustachesWithHtmlElements.apply($(this));
   });
 };
 
+function insertTagsWrapper() {
+  var tagListHtml = "<div class='js-cds-tag-list cds-tag-list'></div>";
+  $(".list-card-title").each(function() {
+    $(tagListHtml).insertAfter(this);
+  });
+}
+
 $(function(){
-  convertHashtagsToBadge()
+  insertTagsWrapper();
+  convertTagsToBadge()
 });
